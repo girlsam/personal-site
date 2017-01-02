24544954a4aed2285c58f069239416eb3182898f
Sam Goldsmith <samggold@gmail.com>
2017-01-02 16:09:39 -0700

    misc to gitignore

--- misc/content.js
+++
@@ -1,5 +0,0 @@
-// var proj1 = "1 Boop";
-// var proj2 = "2 Boop";
-// var proj3 = "3 Boop";
-// var proj4 = "4 Boop"
-// var proj5 = "5 Boop";
--- misc/social-icons/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/thalseth/pen/saqIj.
-
- Round social sharing buttons
